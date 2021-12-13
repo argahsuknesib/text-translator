7c498b11b21f6ed3ec6c22611f40f2cbf25bbcc3
Kushagra Singh Bisen <k.bisen@yahoo.com>
2021-12-13 22:13:54 +0100

    translator app is done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import React, {useState} from "react";
+import Field from "./components/field";
+import Languages from "./components/languages";
+import Translate from "./components/translate";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    const [language, setLanguage] = useState('ar');
+    const [text, setText] = useState("");
+    
+    return (
+        <div>
+            <Field label = "Enter English" onChange = {setText} value = {text} />
+            <Languages language = {language} onLanguageChange = {setLanguage} />
+            <hr />
+            <Translate text = {text} language = {language} />
+        </div>
+    )
 }
-
-export default App;
